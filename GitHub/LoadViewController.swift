9bfc2fed5fd8394695638bfceb278b737bbf9662
SudheerBabuA <Sudheerbabu.4888@gmail.com>
2022-11-21 11:27:28 +0530

    load

---
+++ GitHub/LoadViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoadViewController.swift
+//  GitHub
+//
+//  Created by Avudari Sudheer Babu on 21/11/22.
+//
+
+import UIKit
+
+class LoadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func load() {
+        print("loaded")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
